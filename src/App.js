7f743cd72f75129b4deb421dacc4654d6098af86
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-11-19 19:53:57 +0200

    React Router Dom:    Added the react router package and linked the 2 pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
-import LandingPage from "./Components/LandingPage";
+import { LandingPage } from "./Components/LandingPage";
+import { QuestionsPage } from "./Components/QuestionsPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <main>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="questions" element={<QuestionsPage />} />
+      </Routes>
+    </main>
   );
 }
 
