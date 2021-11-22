cc9222182b8560c79de54235e2e08c8c49b5bc94
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-11-22 06:48:49 +0200

    Pages added:     Added and integrated pages.

---
+++ src/Pages/FinalPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function FinalPage() {
+  return (
+    <div className="row p-5 ">
+      <div className="col-12 p-2 text-center">
+        <h1>Your answers are submitted successfully</h1>
+      </div>
+    </div>
+  );
+}
--- src/Pages/QuestionsPage.jsx
+++ src/Pages/QuestionsPage.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SPAContext } from "../Context/SPAContext";
 import { Link } from "react-router-dom";
+import Section1 from "../Components/Section1";
+import Section2 from "../Components/Section2";
+import Review from "../Components/Review";
 
 export function QuestionsPage() {
+  const { sectionId } = useContext(SPAContext);
   return (
-    <div className="row p-5 d-flex justify-content-center">
-      <div className="col-6 d-grid gap-2">
-        <Link to="/" className=" btn btn-primary">
-          Exit
-        </Link>
+    <>
+      {sectionId === 1 && (
+        <div className="row p-5 d-flex justify-content-center">
+          <div className="col-6 card">
+            <Section1 />
+          </div>
+        </div>
+      )}
+      {sectionId === 2 && (
+        <div className="row p-5 d-flex justify-content-center">
+          <div className="col-6 card">
+            <Section2 />
+          </div>
+        </div>
+      )}
+
+      {sectionId === 3 && (
+        <div className="row p-5 d-flex justify-content-center">
+          <div className="col-6 card">
+            <Review />
+          </div>
+        </div>
+      )}
+
+      <div className="row p-1 d-flex justify-content-center">
+        <div className="col-6 d-grid gap-2">
+          <Link to="/" className=" btn btn-primary">
+            Go to main page
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
